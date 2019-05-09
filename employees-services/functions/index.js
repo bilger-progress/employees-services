8d6f9c6ea60503687d5e8d1a3d18ab0608c0bcee
bilger-progress <bilger.yahov@progress.com>
2019-05-09 20:31:51 +0300

    Make sure to validate tokens.

--- employees-services/functions/index.js
+++ employees-services/functions/index.js
@@ -1,19 +1,31 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const cors = require("cors")({
+    origin: true,
+});
 
 const getAll = require("./src/getAll.js");
 const getOne = require("./src/getOne.js");
 
-exports.getAll = functions.https.onRequest((request, response) => {
-    return handleHTTPS(request, response, getAll);
-});
+function initializeFirebaseAdminSDK(firebaseAdminSDKConfig) {
+    firebaseAdminSDKConfig.private_key = firebaseAdminSDKConfig.private_key
+        .replace(new RegExp("space", "g"), " ")
+        .replace(/\\n/g, "\n");
+    admin.initializeApp({
+        credential: admin.credential.cert(firebaseAdminSDKConfig),
+        databaseURL: "https://employees-70f8e.firebaseio.com"
+    });
+}
+initializeFirebaseAdminSDK(firebase.config().admin);
 
-exports.getOne = functions.https.onRequest((request, response) => {
-    return handleHTTPS(request, response, getOne);
-});
+exports.getAll = functions.https.onRequest((request, response) =>
+    cors(request, response, () => handleHTTPS(request, response, getAll)));
 
+exports.getOne = functions.https.onRequest((request, response) =>
+    cors(request, response, () => handleHTTPS(request, response, getOne)));
+    
 function handleHTTPS(request, response, handler) {
     return handler(request)
-        .then((data) => {
-            return response.status(200).json(data);
-        });
+        .then(data => response.status(200).json(data))
+        .catch(error => response.status(400).json({error: error.message}));
 }
