49c4f4c9d4e1fb4c5a5435fa12e6459f84b3daa1
bilger-progress <bilger.yahov@progress.com>
2019-05-10 16:21:34 +0300

    Implement Integration Tests.

---
+++ employees-services/functions/test/integration/getOne.test.js
@@ -0,0 +1,43 @@
+"use strict"
+
+const assert = require("assert");
+const rp = require("request-promise");
+
+const getOne = require("../../src/getOne");
+const configs = require("../shared/configs");
+
+const TEST_USER = {
+    email: '',
+    pw: ''
+};
+
+describe("Integration tests for the 'getOne' function.", () =>
+    describe("Should test getting an employee from the backend service.", () =>
+        it("Will fetch the employee, and verify that the object has properties.", () =>
+            rp({
+                method: "POST",
+                uri: configs.IDENTITY_SERVICE + '?key=' + configs.API_KEY,
+                body: {
+                    email: TEST_USER.email,
+                    password: TEST_USER.pw,
+                    returnSecureToken: true
+                },
+                json: true,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(response => rp({
+                    method: "GET",
+                    uri: configs.SERVICE_URL + "/getOne?id=" + configs.TEST_EMPLOYEE_ID,
+                    json: true,
+                    headers: {
+                        "Authorization": `Bearer ${response.idToken}`,
+                        "Content-Type": "application/json"
+                    }
+                }))
+                .then(serviceResponse => assert.notEqual(Object.keys(serviceResponse).length, 0))
+                .catch(error => assert.fail(error))
+        )
+    )
+);
