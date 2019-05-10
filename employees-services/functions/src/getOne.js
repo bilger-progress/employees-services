964485db29093df4b60b9efe9bca423cf2bbc82f
bilger-progress <bilger.yahov@progress.com>
2019-05-10 12:17:15 +0300

    Return some more details for each employee.

--- employees-services/functions/src/getOne.js
+++ employees-services/functions/src/getOne.js
@@ -6,5 +6,17 @@ module.exports = (request) =>
     new Promise((resolve, reject) =>
         admin.auth()
             .verifyIdToken(request.get("Authorization").split("Bearer ")[1])
-            .then(() => resolve(admin.database().ref("employees").child((request.query.id) - 1).once("value")))
+            .then(() => admin.database().ref("employees").child((request.query.id) - 1).once("value"))
+            .then((employeeSnap) => employeeSnap.val())
+            .then(employeeVal => resolve({
+                ...employeeVal,
+                age: `${employeeVal.first_name}'s age`,
+                birth_date: `${employeeVal.first_name}'s birth date`,
+                title: `${employeeVal.first_name}'s title`,
+                gender: `${employeeVal.first_name}'s gender`,
+                income_tax: `${employeeVal.first_name}'s income tax`,
+                national_insurance: `${employeeVal.first_name}'s national insurance`,
+                salary: `${employeeVal.first_name}'s salary`,
+                take_home: `${employeeVal.first_name}'s take home`,
+            }))
             .catch((error) => reject(error)));
